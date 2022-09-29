84c51412f909d247d0595d054f9277845b445068
Coffee Pawz <lauralynneduffy@gmail.com>
2022-09-28 21:18:57 -0400

    Added a form section for user to input desired number of characters; linked user input to variables in JavaScript file

--- Develop/script.js
+++ Develop/script.js
@@ -5,11 +5,34 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+function generatePassword () {
+
+ let node = document.querySelector("body").thirdChild;
+
+  const A = document.getElementById ('minChar').value;
+  const  B = document.getElementById('maxChar').value;
+  const C = document.getElementById('lowChar').value;
+  const D = document.getElementById('upChar').value;
+  const E = document.getElementById('spcChar').value;
+
+  return (B);
+
+
+}
