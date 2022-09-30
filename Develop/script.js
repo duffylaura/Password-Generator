081bf39d289db4d7d69d51610dd025aff1df36f2
Coffee Pawz <lauralynneduffy@gmail.com>
2022-09-29 22:13:47 -0400

    Remaining javascript added and tested.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
-var upALL = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowALL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numALL = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var spcALL = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '/', ']', '^', '_', '`', '{', '|', '}', '~' ];
-var all = upALL.concat(lowALL, numALL, spcALL);
+var upALL = ['S','Y','I','P','M','H','V','G','T','D','N','O','E','B','K','F','Z','Q','L','R','C','J','W','X','A','U'];
+var lowALL = ['u','r','q','t','z','k','p','c','b','s','w','h','n','g','f','m','l','y','i','o','x','j','a','d','e','v'];
+var numALL = ['5','9','8','4','1','2','0','6','7','3'];
+var spcALL = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '/', ']', '^', '_', '`', '{', '|', '}', '~' ];
+var all = ['*','=','>','7','f','%','L','i','~','o','Z','J','S','!','_','5','G','C',',','&','W','1','X','b','d','z','H','I','j','|','k','v','O','0','h','r','N','+','a','B','p','x','/','M','9','Q','R','E','}','D','K','/','c','g','2','e',')','w','A','[','6',';','-','V','@','l','Y','y','P','?','.','$','t','8','q','n','#','4','u','m','<',']','U','{','s','^',':','(','F','T','3','`'];
 
 // Assignment Code
 
@@ -30,97 +30,227 @@ function generatePassword () {
   var num = document.getElementById('numChar').value;
   var spc = document.getElementById('spcChar').value;
 
- // Generate Lowercase Characters, store in randomLowChars
+  //Ensuring that min val is < max val. Used Math.abs because without it, when I would input 8 as min and 148 as max, it wouldn't work. 
 
-  for (var index1 = low; index1 >0; index1--) {
+  if (((min>1) && (max>1)) && (Math.abs(min) > Math.abs(max))){
+    return ((alert ("Logic Error: The minimum character count cannot be larger than the maximum character count.")),"Adjust your criteria entries, then try again.")};
     
-    var rndIndex1 = Math.floor(Math.random()*index1+1);
+  // Generate Lowercase Characters, store in randomLowChars
 
-    var lowALLTemp = lowALL[rndIndex1];
+var randomLowChars = [''];
 
-    lowALL[rndIndex1] = lowALL[index1];
-
-    lowALL [index1] = lowALLTemp;
-  }
+if (low>0){
   
-  var randomLowChars = lowALL.slice(0,low);
+  var alphaRandomStart = Math.round(Math.random()*10);
+  
+  for (var i = low; i>0; i--) {
+  var addTo = lowALL [alphaRandomStart+(i-1)];
+  randomLowChars.push(addTo);}
+
+    // The second part of this "if" statement is to splice out the '' space the array begins with. 
+    // (Not a truly empty array)
+    // Then replace that '' with a random letter
+    // This also adds more variety
+
+  randomLowChars.shift();
+
+  var alphaRandomStart2 = Math.round(Math.random()*10);
+  randomLowChars.push(lowALL[alphaRandomStart2]);
+  randomLowChars.pop();
+};
 
 // Generate Uppercase Characters, store in randomUpChars
 
-for (var index2 = up; index2 >0; index2--) {
-    
-  var rndIndex2 = Math.floor(Math.random()*index2+1);
+var randomUpChars = [''];
 
-  var upALLTemp = upALL[rndIndex2];
+if (up>0){
 
-  upALL[rndIndex2] = upALL[index2];
+  var alphaRandomStart3 = Math.round(Math.random()*10);
 
-  upALL[index2] = upALLTemp;
-}
+  for (var i = up; i>0; i--) {
+  var addTo1 = upALL [(alphaRandomStart3)+(i-1)];
+  randomUpChars.push(addTo1);}
 
-var randomUpChars = upALL.slice(0,up);
+  randomUpChars.shift();
+
+  var alphaRandomStart4 = Math.round(Math.random()*10);
+  randomUpChars.push(upALL[alphaRandomStart4]);
+  randomUpChars.pop();
+};
 
 // Generate Neumeric Characters, store in randomNumChars
 
-for (var index3 = num; index3 >0; index3--) {
-    
-  var rndIndex3 = Math.floor(Math.random()*index3+1);
+var randomNumChars = [''];
 
-  var numALLTemp = numALL[rndIndex3];
+if (num>0){
 
-  numALL[rndIndex3] = numALL[index3];
+  var numRandomStart = Math.round(Math.random()*10);
 
-  numALL[index3] = numALLTemp;
-}
+  for (var i = num; i>0; i--) {
+  var addTo2 = numALL [(numRandomStart)+(i-1)];
+  randomNumChars.push(addTo2);}
 
-var randomNumChars = numALL.slice(0,num);
+  randomNumChars.shift();
+
+  var numRandomStart2 = Math.round(Math.random()*10);
+  randomNumChars.push(numALL[numRandomStart2]);
+  randomNumChars.pop();
+};
 
 // Generate Special Characters, store in randomSpcChars
 
-for (var index4 = spc; index4 >0; index4--) {
+var randomSpcChars = [''];
+
+if (spc>0){
+
+  var spcRandomStart = Math.round(Math.random()*12);
+
+  for (var i = spc; i>0; i--) {
+  var addTo3 = spcALL [(spcRandomStart)+(i-1)];
+  randomSpcChars.push(addTo3);}
+
+  randomSpcChars.shift();
+
+  var spcRandomStart2 = Math.round(Math.random()*10);
+  randomSpcChars.push(spcALL[spcRandomStart2]);
+  randomSpcChars.pop();
+};
+  
+  var criteria1 = randomLowChars.concat(randomUpChars, randomNumChars, randomSpcChars);
+
+  var criteria2 = criteria1.join('');
+
+//////////////\\\\\\\\\\\\\\\\\\\\\\\
+
+if (criteria2=='') {
     
-  var rndIndex4 = Math.floor(Math.random()*index4+1);
+          if ((min>1)&&(max<1)) {
+              var minInputOnly = [''];
+              var minRandomStart = Math.round(Math.random()*15);
+              
+              for (var i = min; i>0; i--) {
+                var addTo5 = all[(minRandomStart)+(i-1)];
+                minInputOnly.push(addTo5);}
+            
+              var minInputOnlyReturn = minInputOnly.join('');
 
-  var spcALLTemp = spcALL[rndIndex4];
+              return (minInputOnlyReturn);
+          };
 
-  spcALL[rndIndex4] = spcALL[index4];
+          if ((max>1)&&(min<1)) {
+            var maxInputOnly = [''];
+            var maxRandomStart = Math.round(Math.random()*20);
+            
+            for (var i = max; i>0; i--) {
+              var addTo6 = all[(maxRandomStart)+(i-1)];
+              maxInputOnly.push(addTo6);}
+          
+            var maxInputOnlyReturn = maxInputOnly.join('');
 
-  spcALL[index4] = spcALLTemp;
+            return (maxInputOnlyReturn);
+          };
+
+          if ((min==1)&&(max<1)){
+            return ((alert ("A secure password must be longer than 1 character")), "Enter more specific criteria, then try again.")};
+
+          if ((min==1)&&(max>1)){
+            var maxInputOnly = [''];
+            var maxRandomStart = Math.round(Math.random()*20);
+            
+            for (var i = max; i>0; i--) {
+              var addTo6 = all[(maxRandomStart)+(i-1)];
+              maxInputOnly.push(addTo6);}
+          
+            var maxInputOnlyReturn = maxInputOnly.join('');
+
+            return (maxInputOnlyReturn);
+            };
+  };
+
+  if ((min=='') && (max=='')) {
+    return (criteria2)
+  };
+  
+  if (max==1){
+          return ((alert ("A secure password must be longer than 1 character")), "Enter more specific criteria, then try again.")};
+
+  if (min==1){
+    return (criteria2)
+  };
+
+  if ((max>1)&&(min<1)) {
+
+    if ((criteria2.length) <= (max)) {
+      return (criteria2)
+    };
+
+    if ((criteria2.length)> Math.abs(max)) {
+      alert ("The total number of characters you required as criteria is greater than the maximum number of characters that you have allowed.");
+      return ("Check criteria you have entered, then try again.")
+    };
+  };
+  
+  if (min>1) {
+
+        if (criteria2.length >= min) {
+          return (criteria2);
+        };
+        
+        if ((criteria2.length >= min)&&(max>1)) {
+          return (criteria2);
+        };
+
+        if ((criteria2.length-2)<min) {
+          
+          var additionalChars = [''];
+
+          var addRandomStart = Math.round (Math.random()*40);
+
+          var addAmount = min - (criteria2.length-2); 
+
+          for (var i = addAmount; i>0; i--) {
+
+              var addTo4 = all[(addRandomStart)+(i)];
+              additionalChars.push(addTo4);}
+          
+          // Create more variety
+
+          additionalChars.shift();
+
+          var A = Math.round(Math.random()*55);
+
+          additionalChars.push (all[A]);
+
+          additionalChars.pop();
+
+          // Create more variety 
+
+          additionalChars.shift();
+
+          var B = Math.round(Math.random()*20);
+
+          additionalChars.push (all[B]);
+
+          additionalChars.pop();
+
+          //Create more variety 
+
+          additionalChars.shift();
+
+          var C = Math.round(Math.random()*7);
+
+          additionalChars.push (all[C]);
+
+          additionalChars.pop();
+
+          //
+
+          var criteria3 = additionalChars.join('');
+
+          var criteria4 = criteria2.concat(criteria3);
+
+          return (criteria4);
+          };
+    };
+  
 }
-
-var randomSpcChars = spcALL.slice(0,spc);
-
-// Bring everything together //
-
-// something is happening here with concat that takes away randomness //
-
-var criteria1 = randomLowChars.concat(randomUpChars, randomNumChars, randomSpcChars);
-
-if (criteria1.length < (min+1)) {
-
-  for (var index5 = (min-criteria1.length); index5>0; index5--) {
-    
-    var rndIndex5 = Math.floor(Math.random()*index5+1);
-  
-    var allTemp = all[rndIndex5];
-  
-    all[rndIndex5] = all[index5];
-  
-    all[index5] = allTemp;
-  }
-  
-  var MakeTheMinimum = all.slice(0,(min-criteria1.length));
-
-  var criteria2 = criteria1.concat(MakeTheMinimum);
-
-return (criteria2);
-}
-else {return (criteria1)};
-
-}
-
-// need to fix issue with randomness  (line 95)
-// need to add to final "if" statement about if someone enters a maximum amt of char
-// final return use join() method // ex. fruits.join('') // in notebook// 
-// Also when totally finished look up Fisher-Yates shuffle 
-// Add in shuffle if have time? or use the randomizing method I alread have?
